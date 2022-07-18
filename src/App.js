d2bccdd5ce9e0278b06c2b16133cc4e1c256a6c8
Zach Howes <zhowes14@icloud.com>
2022-07-18 00:48:15 -0400

    sends http request when cart updates

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => {
     return state.ui.cartIsVisable;
   });
+
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch("https://react-http-87980-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
